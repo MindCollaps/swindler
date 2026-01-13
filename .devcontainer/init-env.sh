d353f298c6f7563e09c74857f62fe92d7211757b
CrazyTim71 <118295691+CrazyTim71@users.noreply.github.com>
2026-01-13 21:22:49 +0000

    fixes

--- .devcontainer/init-env.sh
+++ .devcontainer/init-env.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-# Exit if .env already exists
 if [ -f .env ]; then
   echo ".env file already exists. Skipping generation."
   exit 0
 fi
 
-# enerate random alphanumeric strings (32 chars)
+# generate random alphanumeric strings (32 chars)
 generate_secret() {
-  # tr: delete everything NOT in the set A-Z, a-z, 0-9
-  # head: take the first 32 characters
   tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32
 }
 
@@ -19,9 +16,7 @@ POSTGRES_USER="swindler"
 POSTGRES_DB="swindler"
 POSTGRES_PASSWORD=$(generate_secret)
 REDIS_PASSWORD=$(generate_secret)
-ADMIN_USERNAME=system
 ADMIN_PASSWORD=$(generate_secret)
-ADMIN_EMAIL=john.smith@example.com
 
 cat > .env <<EOF
 POSTGRES_USER=${POSTGRES_USER}
@@ -29,6 +24,9 @@ POSTGRES_DB=${POSTGRES_DB}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
 REDIS_PASSWORD=${REDIS_PASSWORD}
 DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/${POSTGRES_DB}
+ADMIN_USERNAME=system
+ADMIN_PASSWORD=${ADMIN_PASSWORD}
+ADMIN_EMAIL=john.smith@example.com
 EOF
 
 echo ".env file created successfully."
